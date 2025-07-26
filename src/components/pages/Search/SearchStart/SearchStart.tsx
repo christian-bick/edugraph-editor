786c09208cf0cb91c0d09fc175251e6c900c8c06
Christian Bick <christian.bick@bitsuppliers.com>
2025-07-26 23:45:44 +0200

    Improve Search Start dialogue

--- src/components/pages/Search/SearchStart/SearchStart.tsx
+++ src/components/pages/Search/SearchStart/SearchStart.tsx
@@ -1,17 +1,22 @@
 import './SearchStart.css'
 import React from 'react';
-import icon from '../../../../assets/icons/add_photo.svg'
+import icon_photo from '../../../../assets/icons/take_photo.svg'
+import icon_upload from '../../../../assets/icons/upload.svg'
 
 export const SearchStart = () => (
     <div className="search-start">
         <div className="search-prompt">
-            Find Similar
+            Share Example
         </div>
         <div className="search-form">
             <label htmlFor="upload-input">
-                <img src={icon} alt="Scan"/>
+                <img src={icon_photo} alt="Photo"/>
+                <img src={icon_upload} alt="Scan"/>
             </label>
             <input id="upload-input" type="file" accept="image/*" capture="environment"/>
+        </div>
+        <div className="search-prompt">
+            to find similar
         </div>
     </div>
 )
