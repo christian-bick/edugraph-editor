bf9c2ff0860ba79a7d90ecdcf0a56974c711d9a5
Christian Bick <christian.bick@bitsuppliers.com>
2025-07-27 20:33:41 +0200

    Add DimensionFilter

--- src/components/pages/Search/SearchFilter/SearchFilter.tsx
+++ src/components/pages/Search/SearchFilter/SearchFilter.tsx
@@ -2,11 +2,17 @@ import './SearchFilter.scss'
 import React from 'react';
 import {SectionHeader} from "../../../global/SectionHeader/SectionHeader.tsx";
 import {MetaFilter} from "./MetaFilter/Metafilter.tsx";
+import {DimensionFilter} from "./DimensionFilter/DimensionFilter.tsx";
 
 export const SearchFilter = () => ([
     <div className="search-filter">
-        <SectionHeader>Meta</SectionHeader>
-        <MetaFilter />
-        <SectionHeader>Area</SectionHeader>
+        <div className="filter-section">
+            <SectionHeader>Meta</SectionHeader>
+            <MetaFilter/>
+        </div>
+        <div className="filter-section">
+            <SectionHeader>Area</SectionHeader>
+            <DimensionFilter dimension="1" label="Addition"/>
+        </div>
     </div>
 ])
