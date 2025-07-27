1273e9348042c01a2f3e58ff09ea810ad0c411e4
Christian Bick <christian.bick@bitsuppliers.com>
2025-07-27 20:07:29 +0200

    Add MetaFilter

---
+++ src/components/pages/Search/SearchFilter/MetaFilter/Metafilter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './MetaFilter.scss'
+
+export const MetaFilter = () => (
+    <select className="meta-filter">
+        <option>Exercise</option>
+        <option>Instruction</option>
+    </select>
+)
--- src/components/pages/Search/SearchFilter/SearchFilter.tsx
+++ src/components/pages/Search/SearchFilter/SearchFilter.tsx
@@ -1,11 +1,12 @@
 import './SearchFilter.scss'
 import React from 'react';
 import {SectionHeader} from "../../../global/SectionHeader/SectionHeader.tsx";
+import {MetaFilter} from "./MetaFilter/Metafilter.tsx";
 
 export const SearchFilter = () => ([
     <div className="search-filter">
-        <SectionHeader>
-            Meta
-        </SectionHeader>
+        <SectionHeader>Meta</SectionHeader>
+        <MetaFilter />
+        <SectionHeader>Area</SectionHeader>
     </div>
 ])
