f71ea00bd5dfe814a854c0ac6d68d27b5c85740a
Christian Bick <christian.bick@bitsuppliers.com>
2025-08-29 12:28:56 +0200

    Add preview for search bar

--- src/components/pages/Search/SearchBrowse/SearchBrowse.tsx
+++ src/components/pages/Search/SearchBrowse/SearchBrowse.tsx
@@ -1,5 +1,5 @@
 import './SearchBrowse.scss'
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import input_icon from '../../../../assets/icons/add_photo.svg'
 import {SectionHeader} from '../../../global/SectionHeader/SectionHeader.tsx';
 import {SearchFilter} from '../SearchFilter/SearchFilter.tsx';
@@ -9,7 +9,26 @@ import {useSearchStore} from "../../../../stores/search.ts";
 export const SearchBrowse = () => {
 
     const results = useSearchStore(state => state.results)
-    console.log(results)
+    const input = useSearchStore(state => state.input)
+
+    const [inputContent, setInputContent] = useState<string | ArrayBuffer>("");
+
+    useEffect(() => {
+        const reader = new FileReader();
+
+        // Resolve the promise with the result when the reader has successfully loaded the file
+        reader.onload = (event) => {
+            setInputContent(event.target.result);
+        };
+
+        // Reject the promise if there's an error
+        reader.onerror = (error) => {
+            console.log(error);
+        };
+
+        // Start reading the file
+        reader.readAsDataURL(input.file);
+    }, [input])
 
     return ([
         <div className="search-browse">
@@ -18,7 +37,7 @@ export const SearchBrowse = () => {
             </SectionHeader>
             <div className="search-input">
                 <div className="input-icon">
-                    <img src={input_icon} alt="Input Icon"/>
+                    <img src={inputContent} alt="Input Icon"/>
                 </div>
                 <div className="input-description">
 
@@ -38,4 +57,23 @@ export const SearchBrowse = () => {
         </div>,
         <SearchFilter/>
     ])
+}
+
+function readFileAsDataURL(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        // Resolve the promise with the result when the reader has successfully loaded the file
+        reader.onload = (event) => {
+            resolve(event.target.result);
+        };
+
+        // Reject the promise if there's an error
+        reader.onerror = (error) => {
+            reject(error);
+        };
+
+        // Start reading the file
+        reader.readAsDataURL(file);
+    });
 }
