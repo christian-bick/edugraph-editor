9301c6be0a3c241e5334a7870be405b1b1389752
Christian Bick <christian.bick@bitsuppliers.com>
2025-08-30 15:08:22 +0200

    Add download and exchange svg

--- src/components/pages/Search/SearchBrowse/SearchResultDetail/SearchResultDetail.tsx
+++ src/components/pages/Search/SearchBrowse/SearchResultDetail/SearchResultDetail.tsx
@@ -2,18 +2,24 @@ import './SearchResultDetail.scss'
 import React from 'react';
 import {SearchResult, useSearchStore} from "../../../../../stores/search.ts";
 import close from "../../../../../assets/icons/close.svg";
+import download from "../../../../../assets/icons/download.svg";
 
 interface SearchResultDetailProps {
     result: SearchResult
 }
 
-export const SearchResultDetail = ({ result }: SearchResultDetailProps) => {
+export const SearchResultDetail = ({result}: SearchResultDetailProps) => {
     const setSelectedResult = useSearchStore(state => state.setSelectedResult)
 
     return (
         <div className="search-result-detail">
             <img className="preview-image" src={result.questionImage} alt="Search Result"/>
-            <img className="close-button" onClick={() => setSelectedResult(null)} src={close} alt="Photo"/>
+            <div className="action-buttons">
+                <a href={result.questionDoc} target="_blank">
+                    <img src={download} alt="Download"/>
+                </a>
+                <img onClick={() => setSelectedResult(null)} src={close} alt="Close"/>
+            </div>
         </div>
     )
 }
