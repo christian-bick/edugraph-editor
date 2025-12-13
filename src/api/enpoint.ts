4994e76669828e6701a43addde438e1f744d3168
Christian Bick <christian.bick@bitsuppliers.com>
2025-12-13 14:36:17 +0100

    Change to use google run endpoint

--- src/api/enpoint.ts
+++ src/api/enpoint.ts
@@ -1,1 +1,1 @@
-export const ENDPOINT = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://api.edugraph.io'
+export const ENDPOINT = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://imagine-server-575953891979.europe-west4.run.app/'
