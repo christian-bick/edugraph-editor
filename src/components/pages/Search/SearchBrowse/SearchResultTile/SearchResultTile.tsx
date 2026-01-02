7eb0948bd0d5615545e004840905b591e4a6f5f7
Christian Bick <christian.bick@bitsuppliers.com>
2026-01-02 18:58:46 +0100

    Add hover indication

--- src/components/pages/Search/SearchBrowse/SearchResultTile/SearchResultTile.tsx
+++ src/components/pages/Search/SearchBrowse/SearchResultTile/SearchResultTile.tsx
@@ -7,10 +7,13 @@ interface SearchResultTileProps {
 
 export const SearchResultTile = ({result}: SearchResultTileProps) => {
     const setSelectedResult = useSearchStore(state => state.setSelectedResult)
+    const setHighlightedResult = useSearchStore(state => state.setHighlightedResult)
 
     return (
         <div className="search-result"
              onClick={() => setSelectedResult(result)}
+             onMouseEnter={() => setHighlightedResult(result)}
+             onMouseLeave={() => setHighlightedResult(null)}
         >
             <div className="search-result-content">
                 <img src={result.questionImage} alt="Search Result"/>
