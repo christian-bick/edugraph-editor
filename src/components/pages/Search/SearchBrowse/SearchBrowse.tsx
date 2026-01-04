0f1bcb8ca8d2886fafc06641fee12244b5748165
Christian Bick <christian.bick@bitsuppliers.com>
2026-01-04 21:34:13 +0100

    Optimize responsiveness

--- src/components/pages/Search/SearchBrowse/SearchBrowse.tsx
+++ src/components/pages/Search/SearchBrowse/SearchBrowse.tsx
@@ -33,7 +33,7 @@ export const SearchBrowse = () => {
     const classifyClass = selectedFunction === SearchFunction.Classification ? "active" : "inactive"
 
     return (
-        <>
+        <div className="search-page-grid">
             <div className="search-input">
                 <div className="input-upload">
                     <Link to="/">
@@ -59,5 +59,6 @@ export const SearchBrowse = () => {
                 }
             </div>
             <SearchFilter/>
-        </>)
+        </div>)
 }
+
