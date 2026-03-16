381b279522f28f84de16126553756cba93922d41
Christian Bick <christian.bick@bitsuppliers.com>
2026-03-16 15:24:01 +0100

    Remove devtools

--- src/stores/ontology-store.ts
+++ src/stores/ontology-store.ts
@@ -7,9 +7,8 @@ import {
     getQuadsFromString,
     populateOntologyFromQuads
 } from "./ontology-parser.ts";
-import { temporal } from 'zundo';
-import { devtools } from 'zustand/middleware';
-import { produce } from 'immer';
+import {temporal} from 'zundo';
+import {produce} from 'immer';
 
 const IRI_NAMESPACE = 'http://edugraph.io/edu/';
 
@@ -31,108 +30,111 @@ interface OntologyState {
 }
 
 export const useOntologyStore = create<OntologyState>()(
-    devtools(
-        temporal(
-            (set, get) => ({
-                ontologies: {
-                    Area: null,
-                    Ability: null,
-                    Scope: null,
-                },
-                ontologiesOriginal: {
-                    Area: null,
-                    Ability: null,
-                    Scope: null,
-                },
-                loading: false,
-                error: null,
-                fetchOntology: async (branch: string) => {
-                    if (get().loading) return;
+    temporal(
+        (set, get) => ({
+            ontologies: {
+                Area: null,
+                Ability: null,
+                Scope: null,
+            },
+            ontologiesOriginal: {
+                Area: null,
+                Ability: null,
+                Scope: null,
+            },
+            loading: false,
+            error: null,
+            fetchOntology: async (branch: string) => {
+                if (get().loading) return;
 
-                    set({ loading: true, error: null }, false, 'FETCH');
-                    try {
-                        const fileMapping: Record<string, 'Area' | 'Ability' | 'Scope'> = {
-                            "core-areas-math.ttl": "Area",
-                            "core-abilities.ttl": "Ability",
-                            "core-scopes-math.ttl": "Scope",
+                set({loading: true, error: null}, false, 'FETCH');
+                try {
+                    const fileMapping: Record<string, 'Area' | 'Ability' | 'Scope'> = {
+                        "core-areas-math.ttl": "Area",
+                        "core-abilities.ttl": "Ability",
+                        "core-scopes-math.ttl": "Scope",
+                    };
+                    const files = Object.keys(fileMapping);
+                    const rawTurtles = await loadOntologyFiles(files, branch);
+
+                    const finalOntologies: {
+                        Area: Ontology | null,
+                        Ability: Ontology | null,
+                        Scope: Ontology | null
+                    } = {
+                        Area: null,
+                        Ability: null,
+                        Scope: null,
+                    };
+
+                    for (let i = 0; i < files.length; i++) {
+                        const fileName = files[i];
+                        const rawTurtle = rawTurtles[i];
+                        const type = fileMapping[fileName];
+
+                        const quads = await getQuadsFromString(rawTurtle);
+                        const entityInfoMap = createEntityInfoMap(quads);
+
+                        const newOntology: Ontology = {
+                            entities: [],
+                            relations: {
+                                expands: {},
+                                partOf: {},
+                                includes: {},
+                            },
                         };
-                        const files = Object.keys(fileMapping);
-                        const rawTurtles = await loadOntologyFiles(files, branch);
 
-                        const finalOntologies: { Area: Ontology | null, Ability: Ontology | null, Scope: Ontology | null } = {
-                            Area: null,
-                            Ability: null,
-                            Scope: null,
-                        };
-
-                        for (let i = 0; i < files.length; i++) {
-                            const fileName = files[i];
-                            const rawTurtle = rawTurtles[i];
-                            const type = fileMapping[fileName];
-
-                            const quads = await getQuadsFromString(rawTurtle);
-                            const entityInfoMap = createEntityInfoMap(quads);
-
-                            const newOntology: Ontology = {
-                                entities: [],
-                                relations: {
-                                    expands: {},
-                                    partOf: {},
-                                    includes: {},
-                                },
-                            };
-
-                            populateOntologyFromQuads(newOntology, quads, entityInfoMap);
-                            const finalOntology = enrichOntology(newOntology);
-                            finalOntologies[type] = finalOntology;
-                        }
-                        set({
-                            ontologies: structuredClone(finalOntologies),
-                            ontologiesOriginal: finalOntologies,
-                            loading: false
-                        }, false, 'FETCH');
-                        useOntologyStore.temporal.getState().clear()
-
-                    } catch (error: any) {
-                        set({ error: error.message, loading: false }, false, 'FETCH');
-                        console.error(error);
+                        populateOntologyFromQuads(newOntology, quads, entityInfoMap);
+                        const finalOntology = enrichOntology(newOntology);
+                        finalOntologies[type] = finalOntology;
                     }
-                },
-                updateEntity: (dimension, originalEntity, newId, newDefinition) => {
-                    let newIri: string | undefined;
-                    set(produce((draft: OntologyState) => {
-                        const ontology = draft.ontologies[dimension];
-                        if (!ontology) return;
+                    useOntologyStore.temporal.getState().pause()
+                    set({
+                        ontologies: structuredClone(finalOntologies),
+                        ontologiesOriginal: finalOntologies,
+                        loading: false
+                    });
+                    useOntologyStore.temporal.getState().resume()
 
-                        const oldIri = originalEntity.iri;
-                        newIri = `${IRI_NAMESPACE}${newId}`;
+                } catch (error: any) {
+                    set({error: error.message, loading: false});
+                    console.error(error);
+                }
+            },
+            updateEntity: (dimension, originalEntity, newId, newDefinition) => {
+                let newIri: string | undefined;
+                set(produce((draft: OntologyState) => {
+                    const ontology = draft.ontologies[dimension];
+                    if (!ontology) return;
 
-                        const entityToUpdate = ontology.entities.find(e => e.iri === oldIri);
-                        if (entityToUpdate) {
-                            entityToUpdate.iri = newIri;
-                            entityToUpdate.name = newId;
-                            entityToUpdate.definition = newDefinition;
+                    const oldIri = originalEntity.iri;
+                    newIri = `${IRI_NAMESPACE}${newId}`;
+
+                    const entityToUpdate = ontology.entities.find(e => e.iri === oldIri);
+                    if (entityToUpdate) {
+                        entityToUpdate.iri = newIri;
+                        entityToUpdate.name = newId;
+                        entityToUpdate.definition = newDefinition;
+                    }
+
+                    Object.values(ontology.relations).forEach(relationMap => {
+                        if (relationMap[oldIri]) {
+                            relationMap[newIri] = relationMap[oldIri];
+                            delete relationMap[oldIri];
                         }
-
-                        Object.values(ontology.relations).forEach(relationMap => {
-                            if (relationMap[oldIri]) {
-                                relationMap[newIri] = relationMap[oldIri];
-                                delete relationMap[oldIri];
+                        Object.keys(relationMap).forEach(subjectIri => {
+                            const index = relationMap[subjectIri].indexOf(oldIri);
+                            if (index !== -1) {
+                                relationMap[subjectIri][index] = newIri;
                             }
-                            Object.keys(relationMap).forEach(subjectIri => {
-                                const index = relationMap[subjectIri].indexOf(oldIri);
-                                if (index !== -1) {
-                                    relationMap[subjectIri][index] = newIri;
-                                }
-                            });
                         });
-                    }));
-                    return newIri;
-                },
-            }),
-            {
-                partialize: (state) => ({ ontologies: state.ontologies }),
-            }
-        )
+                    });
+                }));
+                return newIri;
+            },
+        }),
+        {
+            partialize: (state) => ({ontologies: state.ontologies}),
+        }
     )
 );
