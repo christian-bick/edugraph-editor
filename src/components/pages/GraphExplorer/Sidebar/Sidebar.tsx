d21f7a92270bfd383b8ea02fef870ec0b15c0615
Christian Bick <christian.bick@bitsuppliers.com>
2026-03-18 13:58:52 +0100

    Add link adding and removal visuals

--- src/components/pages/GraphExplorer/Sidebar/Sidebar.tsx
+++ src/components/pages/GraphExplorer/Sidebar/Sidebar.tsx
@@ -3,24 +3,59 @@ import {useSelectedEntityStore} from '../../../../stores/selected-entity-store';
 import {useBranchStore} from '../../../../stores/branch-store.ts';
 import './Sidebar.scss';
 import EditIcon from '../../../../assets/icons/edit.svg';
+import LinkAddIcon from '../../../../assets/icons/link_add.svg';
+import LinkRmIcon from '../../../../assets/icons/link_rm.svg';
 import {EditDefinition, EditIri} from '../EditEntity/EditEntity.tsx';
 import {invertRelations, toNaturalName} from '../../../../stores/utils.ts';
 import {useCurrentOntologyStore} from "../../../../stores/ontology-store.ts";
 import {OntologyEntity} from "../../../../types/ontology-types.ts";
 
-const renderRelationSection = (title: string, entities: OntologyEntity[] | undefined) => {
-    if (!entities || entities.length === 0) return null;
+interface RelationSectionProps {
+    title: string;
+    entities: OntologyEntity[] | undefined;
+    isInverse?: boolean;
+    minRelations?: number;
+}
+
+const RelationSection: React.FC<RelationSectionProps> = ({ title, entities, isInverse = false, minRelations = 0 }) => {
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+    if (isInverse && (!entities || entities.length === 0)) {
+        return null;
+    }
+
+    const showRemoveButton = !isInverse && entities && entities.length > minRelations;
+
     return (
         <div className="sidebar-section">
             <div className="sidebar-header">
                 <h3>{title}</h3>
+                {!isInverse && (
+                    <button className="edit-btn" onClick={() => setIsAddModalOpen(true)}>
+                        <img src={LinkAddIcon} alt="Add Relation"/>
+                    </button>
+                )}
             </div>
-            <ul>
-                {entities.map(e => <li key={e.iri}>{toNaturalName(e.name)}</li>)}
-            </ul>
+            {entities && entities.length > 0 && (
+                <ul>
+                    {entities.map(e => (
+                        <li key={e.iri}>
+                            <span>{toNaturalName(e.name)}</span>
+                            {showRemoveButton && (
+                                <button className="remove-btn" onClick={() => console.log('Remove relation')}>
+                                    <img src={LinkRmIcon} alt="Remove Relation"/>
+                                </button>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            )}
+            {/* Modal for adding relation will be handled later */}
         </div>
     );
-}
+};
+
+
 export const Sidebar: React.FC = () => {
     const { selectedEntityIri } = useSelectedEntityStore();
     const { ontologies } = useCurrentOntologyStore();
@@ -85,7 +120,7 @@ export const Sidebar: React.FC = () => {
                             </div>
                             <div className="section-content-wrapper">
                                 <div className="iri-display">
-                                    <span className="iri-id">:</span>
+                                    <span className="iri-namespace">http://edugraph.io/edu/</span>
                                     <span className="iri-id">{selectedEntity.name}</span>
                                 </div>
                             </div>
@@ -105,13 +140,13 @@ export const Sidebar: React.FC = () => {
 
                         {activePerspective === 'Progression' ? (
                             <>
-                                {renderRelationSection('Expands', selectedEntity.relations.expands)}
-                                {renderRelationSection('Expanded By', selectedEntity.relations.expandedBy)}
+                                <RelationSection title="Expands" entities={selectedEntity.relations.expands} />
+                                <RelationSection title="Expanded By" entities={selectedEntity.relations.expandedBy} isInverse />
                             </>
                         ) : (
                             <>
-                                {renderRelationSection('Parents', selectedEntity.relations.partOf)}
-                                {renderRelationSection('Children', selectedEntity.relations.hasPart)}
+                                <RelationSection title="Parents" entities={selectedEntity.relations.partOf} minRelations={1} />
+                                <RelationSection title="Children" entities={selectedEntity.relations.hasPart} isInverse />
                             </>
                         )}
                     </>
